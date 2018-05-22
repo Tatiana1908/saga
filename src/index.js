5a0ac5c46337c0bf4e80c3a301f4b8ec2badc675
Tatiana <tatiana.190896@gmail.com>
2018-05-22 10:08:55 +0300

    try to do router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
 import { Provider } from 'react-redux';
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 import App from './components/app/app';
@@ -13,11 +13,32 @@ import store from './store/index'
 
 
 ReactDOM.render(
+  <Router>
     <Provider store={store}>
       <MuiThemeProvider theme={theme}>
-          <App />
+        <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/users" component={App} />
+      </div>
       </MuiThemeProvider>
     </Provider>
+  </Router>
 ,
   document.getElementById('root'),
 );
+class Home extends  Component{
+  render(){
+    return(
+      <h2>Hi</h2>
+    )
+  }
+}
